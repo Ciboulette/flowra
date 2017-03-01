27ed42d08601031efa4b5d1cc304c4299089a9ac
Louis Cibot <louiscibot@gmail.com>
2017-03-01 15:41:06 +0100

    release 0.0.17

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 'use strict';
 var util = require('util');
-var async = require('async');
-var requiredOptions = ['clientName', 'handleAction'];
+var requiredOptions = ['application_id', 'handleAction', 'api_key'];
 var amqp = require('amqplib');
-
+var request = require('request');
 /**
  * Construct a new FlowError
  */
@@ -30,25 +29,41 @@ function validate(options) {
 
 function Flowra(options) {
     validate(options);
-    this.clientName = options.clientName;
+    this.application_id = options.application_id;
+    this.api_key = options.api_key;
     this.handleAction = options.handleAction;
-    this.user = options.user;
-    this.password = options.password;
-    this.url = "amqp://" + this.user + ":" + this.password + "@95.85.11.126:5672";
-    this._connect();
+    var that = this;
+
+    var options = {
+        url: 'http://flow.dev/api/rabbitmq/' + this.application_id + '?api_token=' + this.api_key,
+        headers: {
+            'Accept': 'application/json'
+        }
+    };
+
+    request(options, function(error, response, body) {
+        const data = JSON.parse(body);
+        if (data.user && data.password) {
+            that.url = "amqp://" + data.user + ":" + data.password + "@localhost:5672/";
+            that._connect();
+        } else {
+            console.log("bad identifiants:" + data.error);
+        }
+    });
 }
 
 /**
  * Construct a new Flowra Consumer
  */
 Flowra.start = function(options) {
+
     return new Flowra(options)
 };
 
 Flowra.prototype.success = function(message) {
     var that = this;
     var response = {
-      data: message
+        data: message
     }
     that.channel.sendToQueue(that.msg.properties.replyTo, Buffer(JSON.stringify(response)), {correlationId: that.msg.properties.correlationId});
     that.channel.ack(that.msg);
@@ -57,9 +72,9 @@ Flowra.prototype.success = function(message) {
 Flowra.prototype.failure = function(message) {
     var that = this;
     var response = {
-      error: {
-        detail: message
-      }
+        error: {
+            detail: message
+        }
     }
     that.channel.sendToQueue(that.msg.properties.replyTo, Buffer(JSON.stringify(response)), {correlationId: that.msg.properties.correlationId});
     that.channel.ack(that.msg);
@@ -72,21 +87,18 @@ Flowra.prototype._connect = function() {
             connection.close();
         });
         return connection.createChannel().then(function(channel) {
-            var ok = channel.assertQueue(that.clientName, {durable: true});
+            var ok = channel.assertQueue(that.application_id, {durable: true});
             ok.then(function() {
                 channel.prefetch(1);
-                return channel.consume(that.clientName, function(msg) {
+                console.log(' [x] Awaiting for Flow Server');
+                return channel.consume(that.application_id, function(msg) {
                     var data = JSON.parse(msg.content.toString());
-                    if (that.clientName == data.client) {
-                        that.channel = channel;
-                        that.msg = msg;
-                        that._processMessage(data, that.handleAction);
-                    }
+                    that.channel = channel;
+                    that.msg = msg;
+                    that._processMessage(data, that.handleAction);
                 });
             });
-            return ok.then(function() {
-                console.log(' [x] Awaiting for Flow Server');
-            });
+            return ok.then(function() {});
         })
     }).catch(console.warn);
 };
