f47e9d32f9e94c2d1e446ad5a399ba317c77483a
Louis Cibot <louiscibot@gmail.com>
2017-03-06 14:50:51 +0100

    release 0.0.20

--- config/index.js
+++ config/index.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 
 
 module.exports = {
-    url: process.env.URL_API || "http://www.barbouze.fr/api/",
+    url: process.env.URL_API || "http://www.flowra.io/api",
     env: process.env.APP_ENV || "prod",
-    rabbitMQ_ip: process.env.RABBITMQ_IP || "146.185.190.132",
+    rabbitMQ_ip: process.env.RABBITMQ_IP || "188.166.74.245",
     rabbitMQ_port: process.env.RABBITMQ_PORT || "5672"
 };
